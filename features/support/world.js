fa023374179997c7aab722db195ea8773d4bfe31
Tharushi <t.rathnayaka1995@gmail.com>
2024-06-23 12:33:48 +0530

    completed Scripts

--- features/support/world.js
+++ features/support/world.js
@@ -5,7 +5,39 @@ const { puppeteer } = require("puppeteer");
 class CustomWorld {
   async launchBrowser() {
     this.browser = await puppeteer.launch({ headless: true });
+    this.page = await this.browser.newPage();
   }
+
+  async closeBrowser() {
+    await this.browser.close();
+  }
+
+  async visit() {
+    await this.page.goto("https://practicesoftwaretesting.com/#/auth/login");
+  }
+
+  async enterUsername() {
+    await this.page.waitForSelector(`#email`);
+    await this.page.type(`#email`, `admin@practicesoftwaretesting.com`);
+  }
+
+  async enterPassword() {
+    await this.page.waitForSelector(`#password`);
+    await this.page.type(`#password`, `welcome01`);
+  }
+
+  async submitLogin() {
+    await this.page.click(`input[value='Login']`);
+
+    const urlPage = await page.url();
+
+    expect(urlPage).to.be.a(
+      "string",
+      "https://practicesoftwaretesting.com/#/account"
+    );
+  }
+
+  async VerifySuccessLogin() {}
 }
 
 setWorldConstructor(CustomWorld);
---
+++ report.js
@@ -0,0 +1,11 @@
+const reporter = require("cucumber-html-reporter");
+const options = {
+  //themes - bootstrap/hierarchy/foundation/simple
+  theme: "bootstrap",
+  jsonFile: "cucumber_report.json",
+  output: "cucumber_report.html",
+  reportSuiteAsScenarios: true,
+  launchReport: false,
+};
+
+reporter.generate(options);
